2015cab9a2609d347a621ac03b5d3954dc515c06
abdulhay103 <mdabdulhay95@gmail.com>
2020-10-22 23:57:09 +0600

    Add onClick Button

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 let targetedImage = document.getElementById("targetImages");
 let altText = document.getElementById("altText");
-altText.innerHTML = "Textt";
+// altText.innerHTML = "Textt";
 
 function ImageHandler() {
   targetedImage.src = "../images/images1.jpg";
+}
+
+function onclick() {
+  altText.innerHTML = "Button Clicked";
 }
