dc90f7c969fa72e8bb544bfdb4d85eb4c09ce808
abdulhay103 <mdabdulhay95@gmail.com>
2020-10-23 17:04:44 +0600

    Courcesera 2nd Week project setup

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
-let targetedImage = document.getElementById("targetImages");
-let altText = document.getElementById("altText");
-// altText.innerHTML = "Textt";
-
-function ImageHandler() {
-  targetedImage.src = "../images/images1.jpg";
+function upDate(previewPic) {
+  /* In this function you should 
+     1) change the url for the background image of the div with the id = "image" 
+     to the source file of the preview image
+     
+     2) Change the text  of the div with the id = "image" 
+     to the alt text of the preview image 
+     */
 }
 
-function showImage() {
-  altText.innerHTML = "Button Clicked";
-  console.log(targetedImage, altText);
+function unDo() {
+  /* In this function you should 
+     1) Update the url for the background image of the div with the id = "image" 
+     back to the original-image.  You can use the css code to see what that original URL was
+     
+     2) Change the text  of the div with the id = "image" 
+     back to the original text.  You can use the html code to see what that original text was
+     */
 }
