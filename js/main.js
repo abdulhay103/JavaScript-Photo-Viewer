60eeb42b40987c9d3448c80b2c50c0ac7ac3f508
abdulhay103 <mdabdulhay95@gmail.com>
2020-10-23 23:28:48 +0600

    Project Done

--- js/main.js
+++ js/main.js
@@ -1,22 +1,11 @@
 let imgViewer = document.getElementById("image");
 function upDate(previewPic) {
   // console.log(imgViewer);
-  imgViewer.style.backgroundImage = `url(previewPic.src)`;
-  /* In this function you should 
-     1) change the url for the background image of the div with the id = "image" 
-     to the source file of the preview image
-     
-     2) Change the text  of the div with the id = "image" 
-     to the alt text of the preview image 
-     */
+  imgViewer.style.backgroundImage = `url(${previewPic.src})`;
+  imgViewer.innerHTML = previewPic.alt;
 }
 
 function unDo() {
-  /* In this function you should 
-     1) Update the url for the background image of the div with the id = "image" 
-     back to the original-image.  You can use the css code to see what that original URL was
-     
-     2) Change the text  of the div with the id = "image" 
-     back to the original text.  You can use the html code to see what that original text was
-     */
+  imgViewer.style.backgroundImage = `url("")`;
+  imgViewer.innerHTML = "Hover over an image below to display here.";
 }
